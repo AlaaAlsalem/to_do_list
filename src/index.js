e7dbf977d0729ad189d7523232e8bac8238a42fc
AlaaAlsalem <alsalem.aladdin@gmail.com>
2021-10-02 18:23:59 +0300

    Add logic functoin to index.js to fill the list

--- src/index.js
+++ src/index.js
@@ -1,2 +1,49 @@
 
   import './style.css';
+  import _ from 'lodash';
+
+const container = document.querySelector('.list-container');
+alert('it is work')
+const todoList = [
+  {
+    index: 0,
+    description: 'Wash the dishes',
+    completed: true,
+  },
+
+  {
+    index: 1,
+    description: 'Complete todo lis project',
+    completed: true,
+  },
+
+  {
+    index: 2,
+    description: 'Play game with my girlfriend',
+    completed: true,
+  },
+
+  {
+    index: 3,
+    description: 'Eat some cookies',
+    completed: true,
+  },
+];
+
+const display = () => {
+    alert('hello')
+  todoList.forEach((item) => {
+    const containerItems = `
+    <div class="task">
+    <div class="wrapper">
+        <input type="checkbox" name="" class="box m-input" data-id=${item.index} ${item.completed ? 'checked' : ''}>
+        <input type="text" name="" data-id="${item.index}" value="${item.description}">
+    </div>
+        <i id="ellipse" class="fas fa-ellipsis-v m-i"></i>
+        
+    </div>
+    <hr>`;
+    container.innerHTML += containerItems;
+  });
+};
+display();
